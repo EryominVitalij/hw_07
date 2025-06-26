1342e7b7e96ce8004bdddb2e3285c9ace7c987e1
Vitalij Eryomin <Eryomin@bk.ru>
2025-06-26 21:09:48 +0300

    добавил файл path.py cоздание директорий

---
+++ paths.py
@@ -0,0 +1,7 @@
+import os
+
+
+CURRENT_FILE = os.path.abspath(__file__)
+PROJECT_DIR = os.path.dirname(CURRENT_FILE)
+TEMP_DIR = os.path.join(PROJECT_DIR, 'temp')
+RESOURCE_DIR = os.path.join(PROJECT_DIR, 'resource')
